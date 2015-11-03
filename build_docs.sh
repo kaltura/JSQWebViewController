37feef96700eeb9682380497370f219be49420c0
Jesse Squires <jesse.d.squires@gmail.com>
2015-11-02 21:31:43 -0800

    update build script

--- build_docs.sh
+++ build_docs.sh
@@ -6,4 +6,9 @@
 
 git submodule update --remote
 
-jazzy -o ./ --source-directory JSQWebViewController/JSQWebViewController/ --readme JSQWebViewController/README.md -a 'Jesse Squires' -u 'https://twitter.com/jesse_squires' -m 'JSQWebViewController' -g 'https://github.com/jessesquires/JSQWebViewController'
+jazzy --swift-version 2.1 -o ./ --source-directory JSQWebViewController/JSQWebViewController/ \
+      --readme JSQWebViewController/README.md \
+      -a 'Jesse Squires' \
+      -u 'https://twitter.com/jesse_squires' \
+      -m 'JSQWebViewController' \
+      -g 'https://github.com/jessesquires/JSQWebViewController'
