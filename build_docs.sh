5cfd521a50efddc7510278ce39f5dd6d9c016e07
Jesse Squires <jesse.d.squires@gmail.com>
2016-04-07 06:50:41 -0700

    auto-update docs

--- build_docs.sh
+++ build_docs.sh
@@ -6,7 +6,7 @@
 
 git submodule update --remote
 
-jazzy --swift-version 2.2 -o ./ --source-directory JSQWebViewController/JSQWebViewController/ \
+jazzy --swift-version 2.2 -o ./ --source-directory JSQWebViewController/ \
       --readme JSQWebViewController/README.md \
       -a 'Jesse Squires' \
       -u 'https://twitter.com/jesse_squires' \
